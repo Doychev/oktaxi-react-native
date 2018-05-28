04415c7451108e48d739771a9d7c8ff22c74ef47
Martin Doychev <martin.doychev@abv.bg>
2018-05-28 07:05:43 -0700

    home screen setup

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ImageBackground } from 'react-native';
 import { Colors } from '../../Colors.js';
+import MapView from 'react-native-maps';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = { title: 'Home', header: null };
@@ -8,20 +9,42 @@ export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      mapRegion: {
+        latitude: 42,
+        longitude: 37,
+        latitudeDelta: 20,
+        longitudeDelta: 20,
+      },
     };
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
 
+  onRegionChange(region) {
+    this.setState({ mapRegion : region });
+  }
+
+
   componentDidMount() {
+
+  }
+
+  onPressMap = (event) => {
 
   }
 
 
   render() {
     return (
-      <ImageBackground style={styles.container} resizeMode='stretch' source={require('../../images/taxi3.png')}>
+      <View style={styles.container}>
+        <MapView
+          ref={ref => { this.map = ref; }}
+          onPress={e => this.onPressMap(e.nativeEvent)}
+          style={styles.mapStyle}
+          initialRegion={this.state.mapRegion}
+          onRegionChange={this.onRegionChange}>
 
-      </ImageBackground>
+        </MapView>
+      </View>
     );
   }
 
@@ -33,4 +56,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    flex: 1,
+  },
+
 });

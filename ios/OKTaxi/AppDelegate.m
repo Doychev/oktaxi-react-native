d0e2b8b7fb2c318c754640b1fa5ea09c28780236
bs <>
2018-11-26 14:36:19 +0200

    ok

--- ios/OKTaxi/AppDelegate.m
+++ ios/OKTaxi/AppDelegate.m
@@ -23,7 +23,6 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
   [[UIDatePicker appearance] setLocale:[[NSLocale alloc]initWithLocaleIdentifier:@"en_GB"]];
   
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
